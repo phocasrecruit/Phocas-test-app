5ae86f863f19a3efdbc27b2cb6424f2f980adb32
sudddy <suddpro@gmail.com>
2020-10-16 12:54:48 +1300

    initialising the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from "@apollo/client";
+import React from "react";
+import "./App.css";
+import client from "./graphql/apollo";
+import Routes from "./route/route";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
 
 export default App;
