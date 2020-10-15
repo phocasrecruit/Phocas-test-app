43b8e575514bd6214c5e7d741dae7723b3b79a2e
sudddy <suddpro@gmail.com>
2020-10-16 12:58:55 +1300

    index for containers

---
+++ src/container/index.js
@@ -0,0 +1,7 @@
+import CreateBoard from "./CreateBoard/CreateBoard";
+import CreateTicket from "./CreateTicket/CreateTicket";
+import UpdateTicket from "./UpdateTicket/UpdateTicket";
+import ViewTicket from "./ViewTicket/ViewTicket";
+import ViewBoard from "./ViewBoard/ViewBoard";
+
+export { CreateBoard, CreateTicket, UpdateTicket, ViewTicket, ViewBoard };
