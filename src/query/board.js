1fbe4ca35d46f7b1f714b61e38726694bf2b95b8
sudddy <suddpro@gmail.com>
2020-10-16 12:56:03 +1300

    query declaration

---
+++ src/query/board.js
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+
+export const PUT_BOARD = gql`
+  mutation putBoard($organisationId: ID!, $boardId: ID, $input: BoardInput!) {
+    putBoard(
+      organisationId: $organisationId
+      boardId: $boardId
+      input: $input
+    ) {
+      id
+      name
+
+      createdAt
+      updatedAt
+      tickets {
+        name
+        description
+        status
+      }
+    }
+  }
+`;
+
+export const GET_BOARD = gql`
+  query board($organisationId: ID!, $boardId: ID!) {
+    board(organisationId: $organisationId, boardId: $boardId) {
+      id
+      name
+
+      createdAt
+      updatedAt
+      tickets {
+        name
+        description
+        status
+      }
+    }
+  }
+`;
---
+++ src/query/ticket.js
@@ -0,0 +1,50 @@
+import { gql } from "@apollo/client";
+
+export const GET_TICKET = gql`
+  query ticket($organisationId: ID!, $ticketId: ID!) {
+    ticket(organisationId: $organisationId, ticketId: $ticketId) {
+      id
+      name
+      description
+      status
+      visible
+      board {
+        name
+      }
+    }
+  }
+`;
+
+export const PUT_TICKET = gql`
+  mutation putTicket(
+    $organisationId: ID!
+    $boardId: ID!
+    $ticketId: ID
+    $input: TicketInput!
+  ) {
+    putTicket(
+      organisationId: $organisationId
+      boardId: $boardId
+      ticketId: $ticketId
+      input: $input
+    ) {
+      id
+      name
+      description
+      status
+      visible
+    }
+  }
+`;
+
+export const DELETE_TICKET = gql`
+  mutation deleteTicket($organisationId: ID!, $ticketId: ID!) {
+    deleteTicket(organisationId: $organisationId, ticketId: $ticketId) {
+      id
+      name
+      description
+      status
+      visible
+    }
+  }
+`;
